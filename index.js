e7978fb1c1acafcea16503850d7f0416d9b7e09c
Divyashree HS <158317601+Divyashreebabu@users.noreply.github.com>
2025-01-06 13:24:21 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors =require("cors");
 const app = express();
 app.use(express.json());
-const NewsModel require ("./models/news.js");
-app.listen(3000, () => {});
-app.get("/",(req,res)=>{
-res.json({name: "hari" });
+app.use(cors());
+const NewsModel = require ("./models/news.js");
+const SignupModel=require("./models/Signup.js");
+
+app.listen(3001, () => {});
+app.get("/",(req ,res)=>{
+res.json({name: "divya" });
 });
-app.post("/api/addnews",async{req,res}=>{
+app.post("/api/addnews",async(req,res)=>{
     try{
 const news = await NewsModel.create(req.body);
 res.status(200).json(news);
@@ -16,6 +20,33 @@ console.log(req.body);
         res.send(500);
     }
 });
+
+app.post("/api/signup",async(req,res)=>{
+    try{
+const Signup = await SignupModel.create(req.body);
+res.status(200).json(Signup);
+console.log(req.body);
+    }catch (error){
+        res.send(500);
+    }
+});
+const SignupModel = require("./models/Signup");
+
+// Example POST route for sign-up
+app.post("/api/signin", async (req, res) => {
+  try {
+    const newUser = await SigninModel.create(req.body);
+    res.status(201).json(newUser);
+  } catch (error) {
+    if (error.code === 11000) {
+      res.status(400).json({ error: "Email already exists" });
+    } else {
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  }
+});
+
+
 app.get("/api/news",async(req,res)=>{
     try{
         const news=await NewsModel.find({});
@@ -35,7 +66,7 @@ app.get("/api/news",async(req,res)=>{
         res.send(500);
     }
 });
-app.put("/apu/news/:id",async(req,res)=>{
+app.put("/api/news/:id",async(req,res)=>{
     try{
         const{id}=req.params;
         constnews=await NewsModel.findByIdAndUpdate(id,req.body);
@@ -48,7 +79,7 @@ app.put("/apu/news/:id",async(req,res)=>{
         res.send(500);
     }
 });
-app.delete("/apu/news/:id",async(req,res)=>{
+app.delete("/api/news/:id",async(req,res)=>{
     try{
         const{id}=req.params;
         constnews=await NewsModel.findByIdAndUpdate(id,req.body);
@@ -62,11 +93,7 @@ app.delete("/apu/news/:id",async(req,res)=>{
     }
 });
 mongoose
-.connect(mongodb://localhost:27017/
-
-)
+.connect("mongodb://localhost:27017/")
 .then(()=>{
     console.log("connect to db");
-});
-
-
+});    
--- news.js
+++ news.js
@@ -1,5 +1,4 @@
 const mongoose=require("mongoose");
-
 const NewsSchema = mongoose. Schema (
 {
 headline: {
